4e6757a0dedd852feb35a370d0128ce9c3e1789f
dragondmii <robert.c.epstein@gmail.com>
2017-03-04 21:03:58 -0700

    well darn

--- image_retrieval.py
+++ image_retrieval.py
@@ -26,19 +26,26 @@ def mean(v):
 # compute the bgr similarity between
 # two bgr index vectors
 def bgr_img_sim(img_index_vec1, img_index_vec2):
-  # your code
+  b = abs(mean(img_index_vec1[0])-mean(img_index_vec2[0]))
+  g = abs(mean(img_index_vec1[1])-mean(img_index_vec2[1]))
+  r = abs(mean(img_index_vec1[2])-mean(img_index_vec2[2]))
+  yield (b, g, r)
   pass
   
 # compute the hsv similarity between
 # two hsv index vectors
 def hsv_img_sim(img_index_vec1, img_index_vec2):
-  # your code
+  h = abs(mean(img_index_vec1[0])-mean(img_index_vec2[0]))
+  s = abs(mean(img_index_vec1[1])-mean(img_index_vec2[1]))
+  v = abs(mean(img_index_vec1[2])-mean(img_index_vec2[2]))
+  yield (h, s, v)
   pass
 
 # compute the hsv similarity between
 # two gsl index vectors
 def gsl_img_sim(img_index1, img_index2):
-  # your code
+  g = abs(mean(img_index_vec1)-mean(img_index_vec2))
+  yield (g)
   pass
 
 # index the input image
@@ -58,21 +65,55 @@ def index_img(imgp):
 # you do not have to save the index in BGR_INDEX. This index
 # is used to match the indices in the unpickeld BGR_INDEX.
 def index_bgr(img):
-    # your code
+    (B, G, R) = cv2.split(img)
+    (h, w, num_channels) = img.shape
+    bval = 0;
+    gval = 0;
+    rval = 0;
+
+    for hight in xrange(h):
+      for width in xrange(w):
+        bval = bval+B[height][width]
+        gval = gval+G[height][width]
+        rval = rval+R[height][width]
+      output = output + (bval/w, gval/w, rval/w)
+    return output
     pass
 
 # this is very similar to index_hsv in image_index.py except
 # you do not have to save the index in HSV_INDEX. This index
 # is used to match the indices in the unpickeld HSV_INDEX.
 def index_hsv(img):
-    # your code
+    hsv_image =cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    (H, S, V) = cv2.split(hsv_img)
+    (h, w, num_channels) = img.shape
+    hval = 0;
+    sval = 0;
+    vval = 0;
+
+    for hight in xrange(h):
+      for width in xrange(w):
+        hval = hval+H[height][width]
+        sval = sval+S[height][width]
+        vval = vval+v[height][width]
+      output = output + (hval/w, sval/w, vval/w)
+    return output
     pass
 
 # this is very similar to index_gs. in image_index.py except
 # you do not have to save the index in GSL_INDEX. This index
 # is used to match the indices in the unpickeld GSL_INDEX.
 def index_gsl(img):
-    # your code
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    (G) = cv2.split(gray)
+    (h, w, num_channels) = img.shape
+    gbval = 0;
+    
+    for hight in xrange(h):
+      for width in xrange(w):
+        gval = gval+G[height][width]
+      output = output + (bval/w, gval/w, rval/w)
+    return output
     pass
 
 # we will unpickle into these global vars below.
@@ -84,21 +125,36 @@ GSL_INDEX = None
 # index vector and all the vectors in the unpickled
 # bgr index bgr_index and return the top one.
 def compute_bgr_sim(bgr, bgr_index, topn=1):
-  # your code 
+  holding_list = ()
+  for x in xrange(len(bgr_index)):
+    holding_list = list(bgr_img_sim(bgr, bgr_index[x]), bgr_index[x])
+  sort_things = sorted(holding_list,key = holding_list[0] reverse=True)
+  for i in xrange(topn+1):
+    yield(sort_things[i][1])
   pass
 
 # compute the similarities between the hsv
 # index vector and all the vectors in the unpickled
 # hsv index hsv_index and return the top one.
 def compute_hsv_sim(hsv, hsv_index, topn=1):
-  # your code
+  holding_list = ()
+  for x in xrange(len(bgr_index)):
+    holding_list = list(hsv_img_sim(hsv, hsv_index[x]), hsv_index[x])
+  sort_things = sorted(holding_list,key = holding_list[0] reverse=True)
+  for i in xrange(topn+1):
+    yield(sort_things[i][1])
   pass
 
 # compute the similarities between the gsl
 # index vector and all the vectors in the unpickled
 # gsl index gls_index and return the top one.
 def compute_gsl_sim(gsl, gsl_index, topn=1):
-  # your code
+  holding_list = ()
+  for x in xrange(len(gsl_index)):
+    holding_list = list(gsl_img_sim(gsl, gsl_index[x]), gsl_index[x])
+  sort_things = sorted(holding_list,key = holding_list[0] reverse=True)
+  for i in xrange(topn+1):
+    yield(sort_things[i][1])
   pass
 
 # unpickle, match, and display
